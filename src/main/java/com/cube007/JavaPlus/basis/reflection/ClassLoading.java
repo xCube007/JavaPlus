3c5e8a08103c504504bff14e5ac51d4b8ad0f78b
Cube007 <1191727771@qq.com>
2021-07-14 00:14:58 +0800

    类加载测试

---
+++ src/main/java/com/cube007/JavaPlus/basis/reflection/ClassLoading.java
@@ -0,0 +1,27 @@
+package com.cube007.JavaPlus.basis.reflection;
+
+/**
+ * 类加载测试
+ * @author 11917
+ */
+public class ClassLoading {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println("m = " + A.m);
+    }
+}
+
+
+class A {
+
+    // 改变static顺序影响结果
+    static {
+        System.out.println("代码块加载");
+        m = 300;
+    }
+    static int m = 100;
+
+    A() {
+        System.out.println("构造加载");
+    }
+}
