13128dfb76a35e157c6ac0e92541b4b23df610d8
Cube007 <1191727771@qq.com>
2022-06-19 21:42:39 +0800

    升级版本

--- src/main/java/com/cube007/JavaPlus/other/Other.java
+++ src/main/java/com/cube007/JavaPlus/other/Other.java
@@ -1,6 +1,7 @@
 package com.cube007.JavaPlus.other;
 
-import static com.cube007.JavaPlus.algorithm.Other.select;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 11917
@@ -9,8 +10,75 @@ public class Other {
 
     public static void main(String[] args) {
 
-        int[] arr = {1,44,6,32,54,74,89,62,1,4,25,69,74,12,56,98,54,12,365,8,74,1,5,9,93,2,174,23};
-        select(arr);
+        Data data = new Data();
+        new Thread(()->{
+            for(int i = 0; i < 50; i++) {
+                try {
+                    data.printA();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "A").start();
+
+        new Thread(()->{
+            for(int i = 0; i < 50; i++) {
+                try {
+                    data.printB();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "B").start();
+
     }
 
+    public static void print(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+                for(int k = 1; k <= i-1; k++) {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static Map<Character, Integer> statistics(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return map;
+    }
+
+
+}
+
+class Data {
+    private int val = 1;
+    public synchronized void printA() throws InterruptedException {
+        while (val%2 == 0) {
+            this.wait();
+        }
+        System.out.println(val);
+        val++;
+        this.notify();
+    }
+
+    public synchronized void printB() throws InterruptedException {
+        while (val%2 != 0) {
+            this.wait();
+        }
+        System.out.println(val);
+        val++;
+        this.notify();
+    }
 }
